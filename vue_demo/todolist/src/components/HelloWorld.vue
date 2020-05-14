f90cea4fc743d068f878341935609a35349f7c6a
18363007020 <2856527022@qq.com>
2020-05-14 09:16:57 +0800

    first

---
+++ vue_demo/todolist/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="item" @click="deletesss">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    deletesss: function () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+  .item {
+    color: green;
+  }
+</style>
